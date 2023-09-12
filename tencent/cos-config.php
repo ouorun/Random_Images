668a733d6e10f20deee83ff8ba7a7dc21d4e4215
ouorun <140728952+ouorun@users.noreply.github.com>
2023-09-13 02:17:01 +0800

    Create cos-config.php

---
+++ tencent/cos-config.php
@@ -0,0 +1,13 @@
+<?php
+// cos-config.php
+
+return [
+    'region' => 'your_region', // 例如：'ap-shanghai'
+    'credentials' => [
+        'appId' => 'your_app_id',
+        'secretId' => 'your_secret_id',
+        'secretKey' => 'your_secret_key',
+    ],
+    'bucket' => 'your_bucket_name', // 你的存储桶名称
+];
+?>
